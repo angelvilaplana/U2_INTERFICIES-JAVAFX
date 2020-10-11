87bb66255c3388961b2543cb4b4042c9531707f9
Angel Vil <vilaplanaangel@gmail.com>
2020-10-11 23:50:11 +0200

    first commit

---
+++ src/main/java/orihuel/vilaplana/angel/MainApp.java
@@ -0,0 +1,11 @@
+package orihuel.vilaplana.angel;
+
+import orihuel.vilaplana.angel.activitats.PiramideCercles;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        PiramideCercles.main(args);
+    }
+
+}
---
+++ src/main/java/orihuel/vilaplana/angel/activitats/PiramideCercles.java
@@ -0,0 +1,42 @@
+package orihuel.vilaplana.angel.activitats;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class PiramideCercles extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Cercles (LM)");
+        Group root = new Group();
+
+        int widthScreen = 550;
+        int heightScreen = 500;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i; j < 8; j++ ) {
+                int actualX = ((widthScreen / 2) - (j * 30)) + (i * 60);
+                int actualY = (100 + (j * 40));
+                Circle c = new Circle(actualX, actualY, 20);
+                c.setFill(Color.rgb(190, 19, 99));
+                c.setStroke(Color.BLACK);
+                c.setStrokeWidth(3);
+                root.getChildren().add(c);
+            }
+        }
+
+        Scene theScene = new Scene(root, widthScreen, heightScreen);
+        stage.setScene(theScene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
