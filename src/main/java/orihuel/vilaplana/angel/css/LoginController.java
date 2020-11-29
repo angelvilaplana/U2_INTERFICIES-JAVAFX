af12521795d720802a1dd47cf827a3e86964f2ad
Angel Vil <vilaplanaangel@gmail.com>
2020-11-29 23:58:25 +0100

    [MOD] CSS

--- src/main/java/orihuel/vilaplana/angel/css/LoginController.java
+++ src/main/java/orihuel/vilaplana/angel/css/LoginController.java
@@ -1,23 +1,45 @@
 package orihuel.vilaplana.angel.css;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class LoginController {
 
-    private CSSApp app;
+    @FXML
+    private PasswordField tfPass;
 
     @FXML
-    void handleIniciarSesion() throws IOException {
-        String fxml = ".." + File.separator + "controls" + File.separator + "formulari" + File.separator + "FormulariScene.fxml";
-        app.setScene(3, "Formulari", fxml);
+    private TextField tfUserName;
+
+    private CSSApp app;
+
+    private String userName;
+
+    private String password;
+
+    @FXML
+    public void initialize() {
+        userName = "angel";
+        password = "1234";
     }
 
     @FXML
-    void handleRegistrarse() throws IOException {
+    private void handleIniciarSesion() throws IOException {
+        if (tfUserName.getText().equals(userName) && tfPass.getText().equals(password)) {
+            String fxml = ".." + File.separator + "controls" + File.separator + "formulari" + File.separator + "FormulariScene.fxml";
+            app.setScene(3, "Formulari", fxml);
+        } else {
+            tfUserName.setText("");
+            tfPass.setText("");
+        }
+    }
+
+    @FXML
+    private void handleRegistrarse() throws IOException {
         app.setScene(2, "Registration Form", "RegisterView.fxml");
     }
 
